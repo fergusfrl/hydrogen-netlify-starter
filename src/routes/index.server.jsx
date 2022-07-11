7399ccdfc2ec49cfe93d74c29b268a67ab0aec07
Fergus Farrell <fergus@batch.nz>
2022-07-11 15:57:27 +1200

    rollback

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -7,7 +7,6 @@ import {
   useServerAnalytics,
   useLocalization,
   useShopQuery,
-  useQuery,
 } from '@shopify/hydrogen';
 
 import {MEDIA_FRAGMENT, PRODUCT_CARD_FRAGMENT} from '~/lib/fragments';
@@ -29,13 +28,6 @@ export default function Homepage() {
     },
     preload: true,
   });
-
-  const {data: pokemon} = useQuery(['foo', 'bar'], async () => {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-    return await res.json();
-  });
-
-  console.log('POKEMON:', pokemon);
 
   const {heroBanners, featuredCollections, featuredProducts} = data;
 
